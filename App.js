cbe86be28b3d7e254ef1066793c55b419feb67ca
Lang0406 <142191661+Lang0406@users.noreply.github.com>
2023-12-12 18:32:37 +0800

    test

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-// App.js
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { firebase, db } from './firebase'; // Import both firebase and db
+import { firebase, db } from './firebase'; 
 import UserInfo from './UserInfo';
 
 const Stack = createStackNavigator();
@@ -12,35 +12,35 @@ export default function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = async (navigation) => { // Pass navigation as a parameter
+  const handleLogin = async (navigation) => { 
     try {
-      // Sign in user
+     
       const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
       const user = userCredential.user;
 
-      // Retrieve user data from Firestore based on email
+      // Retrieve user data from database
       const querySnapshot = await db.collection('users').where('email', '==', email).get();
 
       if (!querySnapshot.empty) {
-        // Assuming there's only one document for each user (based on email)
+        //one email per account
         const userData = querySnapshot.docs[0].data();
         console.log('User data from Firestore:', userData);
 
-        // Navigate to UserInfoScreen and pass user data as params
+        
         navigation.navigate('UserInfo', { user: userData });
       } else {
         console.log('User data not found in Firestore');
       }
 
-      // Display a pop-up or alert
+      
       Alert.alert('Login Successful', 'Welcome back!');
     } catch (error) {
       console.error('Error logging in:', error.message);
-      // Display an error pop-up or alert
       Alert.alert('Login Error', error.message);
     }
   };
 
+    
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -53,10 +53,11 @@ export default function App() {
   );
 }
 
+//front end
 const LoginScreen = ({ navigation, email, setEmail, password, setPassword, handleLogin }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+      <Text style={styles.title}>TripAid</Text>
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -75,6 +76,25 @@ const LoginScreen = ({ navigation, email, setEmail, password, setPassword, handl
   );
 };
 
+//css
 const styles = StyleSheet.create({
-  // Your styles here
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
 });
